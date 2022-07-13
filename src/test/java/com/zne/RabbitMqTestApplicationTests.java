5546898b7c51ef98915daa80d906e2e7519591c8
zelentre <zelentre@qq.com>
2022-07-13 17:23:10 +0800

    Initial commit

---
+++ src/test/java/com/zne/RabbitMqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
